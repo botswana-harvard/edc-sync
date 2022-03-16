4bb4bd11264995adc82c142e1044095ce9148264
Coulson Kgathi <ckgathi@gmail.com>
2022-03-17 01:43:23 +0200

    update urls

--- edc_sync/urls.py
+++ edc_sync/urls.py
@@ -1,4 +1,4 @@
-from django.urls import  include, path
+from django.urls import  include, re_path
 from edc_constants.constants import UUID_PATTERN
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
@@ -16,16 +16,16 @@ router.register(r'incomingtransaction', IncomingTransactionViewSet)
 app_name = 'edc_sync'
 
 urlpatterns = [
-    path('admin/', edc_sync_admin.urls),
-    path('api/transaction-count/$',
+    re_path(r'^admin/', edc_sync_admin.urls),
+    re_path(r'^api/transaction-count/$',
         TransactionCountView.as_view(), name='transaction-count'),
-    path('dump-to-usb/$',
+    re_path(r'^dump-to-usb/$',
         DumpToUsbView.as_view(), name='dump-to-usb'),
-    path('sync-report/$',
+    re_path(r'^sync-report/$',
         SyncReportView.as_view(), name='sync-report'),
-    path('api/', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token),
-    path('render/(?P<model_name>\w+)/(?P<pk>{})/'.format(UUID_PATTERN.pattern),
+    re_path(r'^api/', include(router.urls)),
+    re_path(r'^api-token-auth/', obtain_auth_token),
+    re_path(r'render/(?P<model_name>\w+)/(?P<pk>{})/'.format(UUID_PATTERN.pattern),
         RenderView.as_view(), name='render_url'),
-    path('', HomeView.as_view(), name='home_url'),
+    re_path(r'^', HomeView.as_view(), name='home_url'),
 ]
