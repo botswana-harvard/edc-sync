9010fecea97c58553673ec8290270c32888c10dc
Itumeleng Mosweu <imosweuk@gmail.com>
2022-05-12 09:07:00 +0200

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-sync',
-    version='0.2.32',
+    version='0.2.33',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
